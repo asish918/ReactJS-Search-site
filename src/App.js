a89ddba49e00a03b4290838ddc50c9c288fc14f7
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-05 23:54:36 +0530

    Implemented static News

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
                     <Rts>
                         <Route path="/" element={<Navigate replace to='/search' />} />
                         <Route path="/search" element={<Results />} />
-                        <Route path="/images" element={<Results />} />
+                        <Route path="/image" element={<Results />} />
                         <Route path="/videos" element={<Results />} />
                         <Route path="/news" element={<Results />} />
                     </Rts>
